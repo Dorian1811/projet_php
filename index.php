ceb8463df6c034602ebe301716a27c926aee8219
Dorian <doriangalant@gmail.com>
2020-04-21 10:26:52 +0200

    projets

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/projet_index.css">
+        <title>LessWheels - Location de voiture</title>
+    </head>
+
+    <body>
+
+
+    </body>
+</html>
